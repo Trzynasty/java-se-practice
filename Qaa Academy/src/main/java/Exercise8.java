d7e2f9fd7d4c9104719dba954f00ad8b027a306f
Adam Kamiński <a.kaminski.business@gmail.com>
2021-09-17 16:43:56 +0200

    commit

---
+++ Qaa Academy/src/main/java/Exercise8.java
@@ -0,0 +1,16 @@
+public class Exercise8 {
+    public static double area(double radius){
+        return radius<0? -1.0: radius*radius*Math.PI;
+    }
+
+    public static double area(double x, double y){
+        return (x<0 || y<0)?-1.0: x*y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(area(5.0));
+        System.out.println(area(-1.0));
+        System.out.println(area(5.0,4.0));
+        System.out.println(area(-1.0,4.0));
+    }
+}
