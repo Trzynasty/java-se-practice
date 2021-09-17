72e643b8ae2c8ec2ceb4ff7dee349f1950021704
Adam Kamiński <a.kaminski.business@gmail.com>
2021-09-17 10:56:13 +0200

    commit

--- Qaa Academy/src/main/java/Exercise1.java
+++ Qaa Academy/src/main/java/Exercise1.java
@@ -1,10 +1,18 @@
 public class Exercise1 {
-    public long toMilesPerHour(double kilometersPerHour){
+    public static long toMilesPerHour(double kilometersPerHour){
         if (kilometersPerHour<0){
             return -1;
         }
         else{
-           return Math.round(kilometersPerHour);
+           return Math.round(kilometersPerHour*0.621);
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toMilesPerHour(1.5));
+        System.out.println(toMilesPerHour(10.25));
+        System.out.println(toMilesPerHour(-5.6));
+        System.out.println(toMilesPerHour(25.42));
+        System.out.println(toMilesPerHour(75.114));
     }
 }
