3c299b38b6cc51d6052d37f647b0c687adfd9065
Adam Kamiński <a.kaminski.business@gmail.com>
2021-09-21 14:14:53 +0200

    commit

---
+++ Qaa Academy/src/main/java/Section5/Exercise16.java
@@ -0,0 +1,22 @@
+package Section5;
+
+public class Exercise16 {
+    public static int sumFirstAndLastDigit(int number){
+        if(number<0) return -1;
+        int firstDigit;
+        int lastDigit=number%10;
+        do {
+            firstDigit=number%10;
+            number/=10;
+        } while(number>0);
+        return firstDigit+lastDigit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumFirstAndLastDigit(252));
+        System.out.println(sumFirstAndLastDigit(257));
+        System.out.println(sumFirstAndLastDigit(0));
+        System.out.println(sumFirstAndLastDigit(5));
+        System.out.println(sumFirstAndLastDigit(-10));
+    }
+}
