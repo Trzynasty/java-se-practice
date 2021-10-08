4f1d5e978b5170cc8e9791afd7ec3fd5dd75c1e6
Adam Kamiński <a.kaminski.business@gmail.com>
2021-10-08 17:38:30 +0200

    commit

---
+++ Qaa Academy/src/main/java/Section6/Rectangle.java
@@ -0,0 +1,33 @@
+package Section6;
+
+public class Rectangle {
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        this.setWidth(width);
+        this.setLength(length);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if(width<0) this.width=0;
+        else this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        if(length<0) this.length=0;
+        else this.length = length;
+    }
+
+    public double getArea(){
+        return width*length;
+    }
+}
