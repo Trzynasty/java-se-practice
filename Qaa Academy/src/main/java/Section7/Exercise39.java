fabc9cda7bf79d036273f58aeabf390c6a94347b
Adam Kamiński <a.kaminski.business@gmail.com>
2021-10-14 15:02:50 +0200

    commit

---
+++ Qaa Academy/src/main/java/Section7/Exercise39.java
@@ -0,0 +1,22 @@
+package Section7;
+
+public class Exercise39 {
+    public static void main(String[] args) {
+        Car car = new Car(8,"Base Car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+        car= new Mitsubishi(6,"Outlander VRX 4WD");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+        car= new Ford(6,"Ford Falcon");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+        car = new Holden(6,"Holden Commodore");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+    }
+}
