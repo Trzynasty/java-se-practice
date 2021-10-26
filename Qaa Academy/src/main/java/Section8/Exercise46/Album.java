3468a2bb9f77290797737375d13035f75da4c474
Adam Kamiński <a.kaminski.business@gmail.com>
2021-10-26 22:28:13 +0200

    commit

--- Qaa Academy/src/main/java/Section8/Exercise46/Album.java
+++ Qaa Academy/src/main/java/Section8/Exercise46/Album.java
@@ -1,4 +1,20 @@
 package Section8.Exercise46;
 
+import java.util.ArrayList;
+
 public class Album {
+    private String name;
+    private String artist;
+    ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        songs = new ArrayList<Song>();
+    }
+
+    public boolean addSong(String name, double duration){
+        songs.add(new Song(name,duration));
+        return true;
+    }
 }
